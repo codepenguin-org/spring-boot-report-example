9c36c5d5599769165fcc5f60dbc4f5d825f46405
Jorge Alfonso García Espinosa <jorgealfonsogarcia@gmail.com>
2017-12-03 13:56:41 -0500

    Initial commit.

---
+++ src/main/java/com/jorgealfonsogarcia/springbootreportexample/SpringBootReportExampleApplication.java
@@ -0,0 +1,12 @@
+package com.jorgealfonsogarcia.springbootreportexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReportExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReportExampleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jorgealfonsogarcia/springbootreportexample/SpringBootReportExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jorgealfonsogarcia.springbootreportexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootReportExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
